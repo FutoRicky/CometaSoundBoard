c18a6f93d86de607d7148010072176ad4d4e7aa8
futoricky <ricardoamercado@gmail.com>
2015-07-23 21:04:07 -0400

    initial commit

---
+++ assets/js/player.js
@@ -0,0 +1,12 @@
+  function EvalSound(soundClip) {
+    var sound = document.getElementById(soundClip);
+    sound.play();
+  };
+
+$(document).ready(function(){
+  
+  $('input').click(function(){
+    console.log('button click happened');
+    EvalSound(this.id);
+  })
+})
